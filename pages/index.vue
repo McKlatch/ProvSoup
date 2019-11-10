8ec41780e804915f85f297d684d64e42fe5267cf
McKlatch <david@hawkandlime.com>
2019-11-10 15:37:11 +0000

    Fix Background width

--- pages/index.vue
+++ pages/index.vue
@@ -1,25 +1,22 @@
 <template>
-  <div class="container mx-auto w-screen h-screen bg-gray-100">
-    <div>
-      <logo class="block mx-auto w-64 h-64 p-3 pt-16" />
-      <h1 class="font-display block font-bold text-6xl text-gray-800 text-center">
-        Proverbial Soup
-      </h1>
-      <h2 class="font-body block text-2xl text-pink-600 text-center">
-        Beneficial sayings worth repeating and sharing in real life (or online)
-      </h2>
-      <div class="block pt-6 pb-16 text-center">
-        <a
-          v-for="social in socialLinks" 
-          v-bind:key="social.id"
-          :href="`${social.url}`"
-          target="_blank"
-          class="inline-block"
-          >
-          <img :src="`${social.image}`"  :alt="`${social.title }`" class="w-12 h-12 p-1" />
-        </a>
-      </div>
-
+  <div>
+    <logo class="block mx-auto w-64 h-64 p-3 pt-16" />
+    <h1 class="font-display block font-bold text-6xl text-gray-800 text-center">
+      Proverbial Soup
+    </h1>
+    <h2 class="font-body block text-2xl text-pink-600 text-center">
+      Beneficial sayings worth repeating and sharing in real life (or online)
+    </h2>
+    <div class="block pt-6 pb-16 text-center">
+      <a
+        v-for="social in socialLinks" 
+        v-bind:key="social.id"
+        :href="`${social.url}`"
+        target="_blank"
+        class="inline-block"
+        >
+        <img :src="`${social.image}`"  :alt="`${social.title }`" class="w-12 h-12 p-1" />
+      </a>
     </div>
   </div>
 </template>
