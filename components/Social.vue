f68968e287fb2077260e100d087b3ce33a907621
McKlatch <david@hawkandlime.com>
2019-11-19 07:19:53 +0000

    Separate some components

---
+++ components/Social.vue
@@ -0,0 +1,28 @@
+<template>
+  <div>
+  <a v-for="social in socialLinks" v-bind:key="social.id" :href="`${social.url}`" target="_blank" class="inline-block">
+    <img :src="`${social.image}`" :alt="`${social.title }`" class="w-12 h-12 p-1" />
+  </a>
+  </div>
+</template>
+<script>
+export default {
+  data() {
+    return {
+      socialLinks: [{
+          id: 0,
+          title: 'Twitter',
+          image: require(`~/assets/img/twitter.svg`),
+          url: 'https://twitter.com/provsoup'
+        },
+        {
+          id: 1,
+          title: 'Instagram',
+          image: require(`~/assets/img/instagram.svg`),
+          url: 'https://www.instagram.com/provsoup'
+        }
+      ]
+    }
+  }
+}
+</script>
--- pages/index.vue
+++ pages/index.vue
@@ -2,36 +2,24 @@
   <div>
     <div class="block pt-4 text-center">
       <logo class="inline-block w-10" />
-      <h1 class="inline-block font-display font-bold text-lg text-gray-800">
-        Proverbial Soup
-      </h1>
     </div>
-      <img
-        :src="displayImgURL"
-        :alt="`&quot;${quote.proverb}&quot; - ${quote.origin}`"
-        class="block mx-auto md:w-1/2 lg:3/4 m-4 shadow-2xl font-body block text-2xl text-gray-800 text-center">
-    <div class="block text-center">
-      <a
-        v-for="social in socialLinks" 
-        v-bind:key="social.id"
-        :href="`${social.url}`"
-        target="_blank"
-        class="inline-block"
-        >
-        <img :src="`${social.image}`"  :alt="`${social.title }`" class="w-12 h-12 p-1" />
-      </a>
-    </div>
+    <img :src="displayImgURL" :alt="`&quot;${quote.proverb}&quot; - ${quote.origin}`" class="block mx-auto md:w-1/2 m-4 shadow-2xl font-body block text-2xl text-gray-800 text-center">
+    <!-- <button class="block text-center bg-gray-300 hover:bg-gray-400 text-gray-800 font-bold py-2 px-4 rounded items-center">
+      <svg width="24" height="24" xmlns="http://www.w3.org/2000/svg" fill-rule="evenodd" clip-rule="evenodd"><path d="M21.67 3.955l-2.825-2.202.665-.753 4.478 3.497-4.474 3.503-.665-.753 2.942-2.292h-4.162c-3.547.043-5.202 3.405-6.913 7.023 1.711 3.617 3.366 6.979 6.913 7.022h4.099l-2.883-2.247.665-.753 4.478 3.497-4.474 3.503-.665-.753 2.884-2.247h-4.11c-3.896-.048-5.784-3.369-7.461-6.858-1.687 3.51-3.592 6.842-7.539 6.858h-2.623v-1h2.621c3.6-.014 5.268-3.387 6.988-7.022-1.72-3.636-3.388-7.009-6.988-7.023h-2.621v-1h2.623c3.947.016 5.852 3.348 7.539 6.858 1.677-3.489 3.565-6.81 7.461-6.858h4.047z"/></svg>
+    </button> -->
+    <social class="block text-center" />
   </div>
 </template>
-
 <script>
 import Logo from '~/components/Logo.vue'
+import Social from '~/components/Social.vue'
 
 export default {
   components: {
-    Logo
+    Logo,
+    Social
   },
-  data () {
+  data() {
     return {
       inQuotes: [],
       quote: {
@@ -40,8 +28,7 @@ export default {
         proverb: 'Loading...',
         source: 'https://proverbialsoup.com/'
       },
-      socialLinks: [
-        {
+      socialLinks: [{
           id: 0,
           title: 'Twitter',
           image: require(`~/assets/img/twitter.svg`),
@@ -57,12 +44,12 @@ export default {
     }
   },
   computed: {
-    quotes () {
+    quotes() {
       return this.inQuotes.filter(function(index) {
         return index.label;
       });
     },
-    displayImgURL () {
+    displayImgURL() {
       if (this.quote.label === "NotLoaded") {
         return require(`~/assets/img/quotes/${this.quote.label}.gif`)
       }
@@ -75,18 +62,18 @@ export default {
       this.inQuotes = inQuotes
       this.genQuote()
     },
-    genQuote () {
+    genQuote() {
       const rand = Math.floor(Math.random() * this.quotes.length)
       const selected = this.quotes[rand]
       this.quote = selected !== this.quote ? selected : this.quotes[0]
     }
   },
-  created () {
+  created() {
     this.fetchSomething()
   }
 }
-</script>
 
+</script>
 <style>
 /* Sample `apply` at-rules with Tailwind CSS
 .container {
