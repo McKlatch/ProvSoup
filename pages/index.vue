4a8a19dd1de2ef4509873015c00aec163368924e
McKlatch <david@hawkandlime.com>
2019-11-19 17:43:32 +0000

    Add randomise button

--- pages/index.vue
+++ pages/index.vue
@@ -3,10 +3,14 @@
     <div class="block pt-4 text-center">
       <logo class="inline-block w-10" />
     </div>
-    <img :src="displayImgURL" :alt="`&quot;${quote.proverb}&quot; - ${quote.origin}`" class="block mx-auto md:w-1/2 m-4 shadow-2xl font-body block text-2xl text-gray-800 text-center">
-    <!-- <button class="block text-center bg-gray-300 hover:bg-gray-400 text-gray-800 font-bold py-2 px-4 rounded items-center">
+    <div class="block text-center m-4">
+    <img :src="displayImgURL" :alt="`&quot;${quote.proverb}&quot; - ${quote.origin}`" class="mx-auto md:w-1/2 shadow-2xl font-body text-2xl text-gray-800">
+    <button
+      class="bg-gray-300 hover:bg-gray-400 text-gray-800 font-bold py-2 px-4 rounded-b-lg items-center shadow-lg"
+      @click="randomQuote()">
       <svg width="24" height="24" xmlns="http://www.w3.org/2000/svg" fill-rule="evenodd" clip-rule="evenodd"><path d="M21.67 3.955l-2.825-2.202.665-.753 4.478 3.497-4.474 3.503-.665-.753 2.942-2.292h-4.162c-3.547.043-5.202 3.405-6.913 7.023 1.711 3.617 3.366 6.979 6.913 7.022h4.099l-2.883-2.247.665-.753 4.478 3.497-4.474 3.503-.665-.753 2.884-2.247h-4.11c-3.896-.048-5.784-3.369-7.461-6.858-1.687 3.51-3.592 6.842-7.539 6.858h-2.623v-1h2.621c3.6-.014 5.268-3.387 6.988-7.022-1.72-3.636-3.388-7.009-6.988-7.023h-2.621v-1h2.623c3.947.016 5.852 3.348 7.539 6.858 1.677-3.489 3.565-6.81 7.461-6.858h4.047z"/></svg>
-    </button> -->
+    </button>
+    </div>
     <social class="block text-center" />
   </div>
 </template>
@@ -60,9 +64,9 @@ export default {
     async fetchSomething() {
       const inQuotes = await this.$axios.$get('https://api.sheety.co/28999f15-3e1a-47f2-83e2-619d2b8e1287')
       this.inQuotes = inQuotes
-      this.genQuote()
+      this.randomQuote()
     },
-    genQuote() {
+    randomQuote() {
       const rand = Math.floor(Math.random() * this.quotes.length)
       const selected = this.quotes[rand]
       this.quote = selected !== this.quote ? selected : this.quotes[0]
