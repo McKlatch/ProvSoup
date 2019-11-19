1ef3b7ef358a06100477653eec5210e1cd157f47
McKlatch <david@hawkandlime.com>
2019-11-19 18:13:18 +0000

    Refactor for better use of layout

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,17 +1,29 @@
 <template>
   <div>
+    <div class="block pt-4 text-center">
+      <logo class="inline-block w-10" />
+    </div>
     <nuxt />
+    <social class="block text-center" />
   </div>
 </template>
 
 <script>
-  export default {
-    head () {
-        return {
-            bodyAttrs: {
-                class: 'container mx-auto w-screen h-screen bg-gray-100'
-            }
-        }
+import Logo from '~/components/Logo.vue'
+import Social from '~/components/Social.vue'
+
+export default {
+  components: {
+    Logo,
+    Social
+  },
+  head() {
+    return {
+      bodyAttrs: {
+        class: 'container mx-auto w-screen h-screen bg-gray-100'
+      }
     }
+  }
 }
+
 </script>
