35bda0ceabfc55651b1910778805d5736f26f71b
McKlatch <david@hawkandlime.com>
2019-11-29 19:54:22 +0000

    Include routes

---
+++ pages/_label.vue
@@ -0,0 +1,15 @@
+<template>
+  <div class="absolute inset-0 flex items-center justify-center">
+    <div class="max-w-sm rounded overflow-hidden">
+      <div>{{$route.params.label}}</div>
+    </div>
+  </div>
+</template>
+<script>
+  export default {
+  validate ({ params }) {
+    // Must be a number
+    return /^[A-Z].*/.test(params.label)
+  }
+}
+</script>
