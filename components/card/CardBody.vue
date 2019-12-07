b607c6fbb632046afee7d68e9cdfd7e94d85e38e
McKlatch <david@hawkandlime.com>
2019-12-07 08:17:54 +0000

    Further separate card into smaller components

---
+++ components/card/CardBody.vue
@@ -0,0 +1,33 @@
+<template>
+  <img class="w-full rounded-tl rounded-tr rounded-bl shadow" :src="displayImgURL" :alt="`&quot;${proverb}&quot; - ${origin}`">
+</template>
+
+<script>
+export default {
+  props: {
+    label: {
+      type: String,
+      required: true,
+      default: 'NotLoaded'
+    },
+    origin: {
+      type: String,
+      required: true,
+      default: 'ProvSoup '
+    },
+    proverb:{
+      type: String,
+      required: true,
+      default: 'Loading...'
+    }
+  },
+  computed: {
+    displayImgURL() {
+      if (this.label === "NotLoaded") {
+        return require(`~/assets/img/quotes/${this.label}.gif`)
+      }
+      return require(`~/assets/img/quotes/${this.label}.png`)
+    }
+  }
+}
+</script>
---
+++ components/card/CardBottom.vue
@@ -0,0 +1,44 @@
+<template>
+  <nuxt-link :to="'/' + randomQuoteLabel">
+      <button class="bg-gray-300 hover:bg-gray-400 text-gray-800 font-bold py-2 px-4 float-right rounded-b shadow">
+        🔀
+      </button>
+  </nuxt-link>
+</template>
+
+<script>
+  export default {
+  props: {
+    label: {
+      type: String,
+      required: true,
+      default: 'NotLoaded'
+    },
+    origin: {
+      type: String,
+      required: true,
+      default: 'ProvSoup '
+    },
+    proverb:{
+      type: String,
+      required: true,
+      default: 'Loading...'
+    },
+    source:{
+      type: String,
+      required: false,
+      default: 'https://proverbialsoup.com/'
+    }
+  },
+  computed: {
+    randomQuoteLabel() {
+      const rdyQuotes = this.$store.getters.readyQuotes
+      const filteredQuotes = rdyQuotes.filter(index => {
+        return index.label !== this.label
+      })
+      const rand = Math.floor(Math.random() * filteredQuotes.length)
+      return filteredQuotes[rand].label
+    }
+  }
+}
+</script>
