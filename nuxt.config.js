b0ce11863b4c10a4005ec9b38a8bf4f2eb11c10c
McKlatch <david@hawkandlime.com>
2020-03-06 12:34:46 +0000

    add awin verification meta tag

--- nuxt.config.js
+++ nuxt.config.js
@@ -9,7 +9,8 @@ export default {
     meta: [
       { charset: 'utf-8' },
       { name: 'viewport', content: 'width=device-width, initial-scale=1' },
-      { hid: 'description', name: 'description', content: process.env.npm_package_description || '' }
+      { hid: 'description', name: 'description', content: process.env.npm_package_description || '' },
+      { name: 'verification', content: 'ebb237a3e04660f87928beda0252d2b8'}
     ],
     link: [
       { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' },
