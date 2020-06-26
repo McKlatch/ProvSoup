228fb3be1331dcb1fb83c7db8a2493b2f5139e91
McKlatch <david@hawkandlime.com>
2020-06-26 21:57:18 +0100

    create error page

---
+++ layouts/error.vue
@@ -0,0 +1,37 @@
+<template>
+  <div class="container">
+    <h1 v-if="statusCode === 404">Page not found</h1>
+    <h1 v-else>An error occurred</h1>
+    <nuxt-link to="/">Home</nuxt-link>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    error: {
+      type: Object,
+      default: null
+    }
+  },
+  computed: {
+    statusCode () {
+      return (this.error && this.error.statusCode) || 500
+    },
+    message () {
+      return this.error.message
+    }
+  },
+  head () {
+    return {
+      title: this.message,
+      meta: [
+        {
+          name: 'viewport',
+          content: 'width=device-width,initial-scale=1.0,minimum-scale=1.0'
+        }
+      ]
+    }
+  }
+}
+</script>
